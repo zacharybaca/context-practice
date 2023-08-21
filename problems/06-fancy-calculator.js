b36e705b9ea482ac7c1482cc73a989e700654f3b
Zachary Baca <zachary.baca@msn.com>
2023-08-21 15:03:41 -0500

    need to add import for calculator

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,5 +1,21 @@
 // Your code here
-
+class FancyCalculator extends Calculator {
+    constructor(total) {
+        super(total);
+    }
+    
+    setTotal(newTotal) {
+        return this.total = newTotal;
+    }
+    
+    modulo(num) {
+        return this.total = this.total % num;
+    }
+    
+    squared() {
+        return this.total *= this.total;
+    }
+}
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
