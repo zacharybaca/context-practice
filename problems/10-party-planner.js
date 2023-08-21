f6ed309f9784a1030b7ed514e101971e3e86806e
zacharybaca <zachary.baca@msn.com>
2023-08-21 17:40:39 -0500

    completed 10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,32 @@
 // Your code here
+class PartyPlanner {
+	constructor() {
+		this.guestList = [];
+	}
 
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
+
+	throwParty() {
+		if (this.guestList.length === 0) {
+			return "Gotta add people to the guest list";
+		} else if (this.guestList.length === 1) {
+			return `Welcome to the party ${this.guestList[0]}`;
+			// let phrase = "Welcome to the party ";
+			// for (let i = 0; i < this.guestList.length; i++) {
+			// 	if (this.guestList.length === 1) {
+			// 		return `Welcome to the party ${t}`
+			// 	}
+			// 	phrase += `${this.guestList[i]} and `
+			// }
+			// return phrase;
+		} else {
+			let lastGuest = this.guestList.pop();
+			return `Welcome to the party ${this.guestList[0]} and ${lastGuest}`;
+		}
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
